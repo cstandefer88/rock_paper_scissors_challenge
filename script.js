91a25e9a63983fab5be44f9e132ad36ff3be5e5c
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-09-22 14:08:30 -0500

    on click of start button, inputs add together and inputs clear

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
-
+  $('#startButton').click(function() {
+    parseInt($('#minuteSet').val() * 60) + parseInt($('#secondSet').val());
+    $('#minuteSet').val('');
+    $('#secondSet').val('');
+  });
 
 
 

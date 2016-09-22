e03e91661434d77f601593e36f05a9e1d6a877ee
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-09-22 15:34:53 -0500

    at 10 seconds, timer alerts user of having 10 seconds left

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ $(document).ready(function() {
     $('#countdown').html((parseInt($('#minuteSet').val() * 60) + parseInt($('#secondSet').val())));;
     $('#minuteSet').val('');
     $('#secondSet').val('');
-
-    var countdown = setInterval(counter, 1000);
-
-    function counter() {
-      if ($('#countdown').html() == 0) {
-        clearInterval();
-    } else {
-        var counter = parseInt($('#countdown').html());
-        counter = counter - 1;
-        $('#countdown').html(counter);
-    }
-    };
-
   });
+
+  var countdown = setInterval(counter, 1000);
+
+  function counter() {
+    var count = parseInt($('#countdown').html())
+
+    if ($('#countdown').html() == 0) {
+      clearInterval();
+    } else if (($('#countdown').html() == 10)) {
+        count = parseInt($('#countdown').html()) - 1;
+        $('#countdown').html(count);
+        alert("You have 10 seconds left");
+      } else {
+          count = parseInt($('#countdown').html()) - 1;
+          $('#countdown').html(count);
+        }
+  };
 
 
 });

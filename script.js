bb7250035e93b0af531f8d0c1234821f52281924
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-09-22 14:56:16 -0500

    timer counts down to 0 then stops upon start button being clicked

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
 
   $('#startButton').click(function() {
-    parseInt($('#minuteSet').val() * 60) + parseInt($('#secondSet').val());
+    $('#countdown').html((parseInt($('#minuteSet').val() * 60) + parseInt($('#secondSet').val())));;
     $('#minuteSet').val('');
     $('#secondSet').val('');
+
+    var countdown = setInterval(counter, 1000);
+
+    function counter() {
+      if ($('#countdown').html() == 0) {
+        clearInterval();
+    } else {
+        var counter = parseInt($('#countdown').html());
+        counter = counter - 1;
+        $('#countdown').html(counter);
+    }
+    };
+
   });
-
-
-
-
 
 
 });

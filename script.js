8af426bb8a8618ae6f76cf5136931df85f38cc93
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-09-23 10:21:24 -0500

    large if-else block changed to switch statement, added comments explaining what each part of the code does

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  // Start and Restart buttons setting chosen value of timer and default scoreboard respectively
+
   $('#startButton').click(function() {
     $('#countdown').html((parseInt($('#minuteSet').val() * 60) + parseInt($('#secondSet').val())));;
     $('span.wins').html('0');
@@ -15,7 +17,16 @@ $(document).ready(function() {
   });
 
 
+
+  // Setting interval of countdown to 1 second
+
   var countdown = setInterval(counter, 1000);
+
+
+
+  // Checking to see if timer is at 0; at which point the timer stops, a winner is declared, and then the scoreboard is set again to default
+  // Checking to see if timer is at 10; at which point an alert is sent off to notify the user of the impending close to the game, and the game continues on
+  // If neither of the above options is the case, the game simply continues
 
   function counter() {
     var count = parseInt($('#countdown').html())
@@ -37,6 +48,9 @@ $(document).ready(function() {
   };
 
 
+
+  // Click events on the buttons allowing the user to have his/her choice submitted followed by the bot making a choice and a winner of the round being added to the scoreboard
+
   $('#rock').click(function() {
         $('#userChoice').html("Rock");
         botChoice();
@@ -56,49 +70,66 @@ $(document).ready(function() {
   });
 
 
+
+  // Bot makes a "choice" move
+
   function botChoice() {
     var bot = Math.random();
+
     if (bot <= 0.33) {
       $('#botChoice').html("Rock");
     } else if (bot >= 0.66) {
         $('#botChoice').html("Paper");
     } else {
         $('#botChoice').html("Scissors");
-    }
+    };
   };
 
+
+
+  // Round winner is determined and the score is tallied into the wins, losses, or ties
 
   function determineRoundWinner() {
 
     var addWin = parseInt($('span.wins').html()) + 1;
     var addLoss = parseInt($('span.losses').html()) + 1;
     var addTie = parseInt($('span.ties').html()) + 1;
+    var results = $('#userChoice').html() + ' and ' + $('#botChoice').html();
 
-    if ($('#userChoice').html() === "Rock" && $('#botChoice').html() === "Scissors") {
-      addWin;
-      $('span.wins').html(addWin);
-    } else if ($('#userChoice').html() === "Rock" && $('#botChoice').html() === "Paper") {
-        addLoss;
-        $('span.losses').html(addLoss);
-    } else if ($('#userChoice').html() === "Paper" && $('#botChoice').html() === "Rock") {
+    switch(results) {
+      case "Rock and Scissors":
         addWin;
         $('span.wins').html(addWin);
-    } else if ($('#userChoice').html() === "Paper" && $('#botChoice').html() === "Scissors") {
+        break;
+      case "Rock and Paper":
         addLoss;
         $('span.losses').html(addLoss);
-    } else if ($('#userChoice').html() === "Scissors" && $('#botChoice').html() === "Paper") {
+        break;
+      case "Paper and Rock":
         addWin;
         $('span.wins').html(addWin);
-    } else if ($('#userChoice').html() === "Scissors" && $('#botChoice').html() === "Rock") {
+        break;
+      case "Paper and Scissors":
         addLoss;
         $('span.losses').html(addLoss);
-    } else {
+        break;
+      case "Scissors and Paper":
+        addWin;
+        $('span.wins').html(addWin);
+        break;
+      case "Scissors and Rock":
+        addLoss;
+        $('span.losses').html(addLoss);
+        break;
+      default:
         addTie;
         $('span.ties').html(addTie);
-    }
-
+    };
   };
 
+
+
+  // A winner of the game is determined based on the total tallies in relation to each other
 
   function declareGameWinner() {
 
@@ -108,13 +139,11 @@ $(document).ready(function() {
 
     if (totalWins > totalLosses && totalTies) {
       alert("You beat the computer this game!  Play again if you want!");
-
     } else if (totalLosses > totalWins && totalTies) {
         alert("The computer beat you this game!  Play again if you want!");
     } else if (totalTies > totalWins && totalLosses) {
         alert("You tied with the computer this game!  Play again if you want!");
     }
   };
-
 
 });
